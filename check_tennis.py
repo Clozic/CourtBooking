f04b6fb3e6bd606a29f941006a75576d9b40889a
Julian <36521497+Clozic@users.noreply.github.com>
2026-04-15 18:42:43 +0200

    Change TARGET_DAYS to a set from environment variable

--- check_tennis.py
+++ check_tennis.py
@@ -6,8 +6,7 @@ from email.mime.text import MIMEText
 from datetime import datetime, timezone
 
 URL = "https://www.tu-sport.de/sportprogramm/kurse/?tx_dwzeh_courses%5Baction%5D=show&tx_dwzeh_courses%5BsportsDescription%5D=768&cHash=302c5e58dded9777b08d1305c1398488"
-
-TARGET_DAYS = int(os.environ.get("TARGET_DAYS", 20))
+TARGET_DAYS = set(os.environ.get("TARGET_DAYS", "").split(","))
 TARGET_START_HOUR = int(os.environ.get("TARGET_START_HOUR", 17))
 TARGET_END_HOUR = int(os.environ.get("TARGET_END_HOUR", 20))
 
